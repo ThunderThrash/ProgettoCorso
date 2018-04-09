3579aa008a654efdc6d8c546c7b8b7af3561eb89
thunder <gianni.amendola@gmail.com>
2018-04-09 23:46:14 +0200

    Settings Activity custom way

---
+++ app/src/main/java/it/frame/progettocorso/Constants.java
@@ -0,0 +1,11 @@
+package it.frame.progettocorso;
+
+public class Constants {
+
+    public static final String SETTINGS_STORAGE = BuildConfig.APPLICATION_ID;
+    public static final String NOTIFY_KEY = ".key.notify";
+    public static final String SPINNER_POS_KEY = ".key.spinner_pos";
+    public static final String USERNAME_KEY = ".key.user_name";
+
+    private Constants(){}
+}
